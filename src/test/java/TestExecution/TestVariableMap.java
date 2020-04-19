abfc6a9cd7a00f24f23c02ff0febe35973929081
Tuyen Nguyen <tuyen.nguyen@hansencx.com>
2020-04-19 20:19:50 +0700

    Add TestVariableMap class

--- src/test/java/TestExecution/TestVariableMap.java
+++ src/test/java/TestExecution/TestVariableMap.java
@@ -1,4 +1,71 @@
 package TestExecution;
 
+import Utils.DateTimeHandler;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.LinkedHashMap;
+
 public class TestVariableMap {
+    private static TestVariableMap instance;
+
+    // This stores default variables such as system date, time, random value
+    private LinkedHashMap<String, Object> commonVariables;
+
+    // This store variables generated in run time such as uniqueId of previous action, then use it for next action
+    // Only available in PRO version
+    private LinkedHashMap<String, Object> processVariables;
+
+    public TestVariableMap() {
+        setCommonVariables(new LinkedHashMap<>());
+        setProcessVariables(new LinkedHashMap<>());
+    }
+
+    public static TestVariableMap getInstance() {
+        if (instance == null) {
+            instance = new TestVariableMap();
+        }
+        return instance;
+    }
+
+    public LinkedHashMap<String, Object> getCommonVariables() {
+        return commonVariables;
+    }
+
+    public void setCommonVariables(LinkedHashMap<String, Object> commonVariables) {
+        this.commonVariables = commonVariables;
+    }
+
+    public LinkedHashMap<String, Object> getProcessVariables() {
+        return processVariables;
+    }
+
+    public void setProcessVariables(LinkedHashMap<String, Object> processVariables) {
+        this.processVariables = processVariables;
+    }
+
+    public void loadCommonVariables() {
+        // Today with date time
+        commonVariables.put("TODAY", DateTimeHandler.currentDayPlus("yyyy-MM-dd_HH-mm-ss", 0));
+
+        // Now with hour minute format
+        commonVariables.put("NOW_HH:mm", DateTimeHandler.currentDayPlus("HH:mm", 0));
+        commonVariables.put("NOW_HHmm", DateTimeHandler.currentDayPlus("HHmm", 0));
+        commonVariables.put("NOW+1_HH:mm", DateTimeHandler.currentDayMinutePlus("HH:mm", 1));
+        commonVariables.put("NOW+1_HHmm", DateTimeHandler.currentDayMinutePlus("HHmm", 1));
+
+        // Date with day month year format
+        commonVariables.put("TODAY_yyyy-MM-dd", DateTimeHandler.currentDayPlus("yyyy-MM-dd", 0));
+        commonVariables.put("YESTERDAY_yyyy-MM-dd", DateTimeHandler.currentDayPlus("yyyy-MM-dd", -1));
+        commonVariables.put("TOMORROW_yyyy-MM-dd", DateTimeHandler.currentDayPlus("yyyy-MM-dd", +1));
+        commonVariables.put("NEXTMONTH_yyyy-MM-dd", DateTimeHandler.currentDayPlus("yyyy-MM-dd", +30));
+
+        // Random with alphabet and number
+        commonVariables.put("RANDOM_alphanumeric_5", RandomStringUtils.randomAlphanumeric(5));
+
+        // Random with number
+        commonVariables.put("RANDOM_numeric_5", Integer.parseInt(RandomStringUtils.randomNumeric(5)));
+
+        // Random with alphabet
+        commonVariables.put("RANDOM_alphabetic_5", RandomStringUtils.randomAlphabetic(5));
+    }
 }
--- src/test/java/Utils/DateTimeHandler.java
+++ src/test/java/Utils/DateTimeHandler.java
@@ -1,4 +1,28 @@
 package Utils;
 
+import org.apache.commons.lang3.time.DateUtils;
+import org.joda.time.DateTime;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateTimeHandler {
+    public static String currentDateAsString(String pattern) {
+        SimpleDateFormat formatter;
+        formatter = new SimpleDateFormat(pattern);
+        return formatter.format(new Date());
+    }
+
+    public static String currentDayPlus(String format, int additional) {
+        Date res = new DateTime(new Date()).plusDays(additional).toDate();
+        SimpleDateFormat formatter = new SimpleDateFormat(format);
+        return formatter.format(res);
+    }
+
+    public static String currentDayMinutePlus(String format, int additional) {
+        Date res = DateUtils.addMinutes(new Date(), additional);
+        SimpleDateFormat formatter = new SimpleDateFormat(format);
+        return formatter.format(res);
+    }
+
 }
