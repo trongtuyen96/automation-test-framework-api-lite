b3e5680ac39d5f7fac74b6f24933544ebeb942b7
Tuyen Nguyen <tuyen.nguyen@hansencx.com>
2020-04-19 14:03:38 +0700

    Add config class

---
+++ src/test/java/config/Config.java
@@ -0,0 +1,21 @@
+package config;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+    public static String getProperty(String property) {
+        Properties properties = new Properties();
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(new File("src\\test\\java\\config.properties"));
+            properties.load(fileInputStream);
+            fileInputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties.getProperty(property);
+    }
+}
